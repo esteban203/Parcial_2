2469f8bad4b8ba077955714eefa3c09ae07eadd0
esteban escobar <esteban203@outlook.com>
2018-05-25 11:59:43 -0500

    git

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-var holi =jasdsjdjasdniad;
+const express = require('express'),
+    engines = require('consolidate');;
+const app = express();
+app.use('/css', express.static('public/css'));
+
+app.engine('hbs', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+app.listen(5100, () => console.log('Example app listening on port 5100!'));
+
+app.get('/', (req, res) => {
+
+    res.render('index');
+
+});
+
+
